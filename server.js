b0cd2d34cc2ef2726f730da7383c51cada0387d2
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-01-10 11:24:15 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,46 @@
-
-const WebSocket = require("ws");
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: 'sql.freedb.tech',
-  user: 'freedb_azizul',
-  password: 'PX*22DY7$mh2mTy',
-  database: 'freedb_ko_database',
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the MySQL database!');
-});
-
-// WebSocket server
+const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
-const users = {}; // Store users and their WebSocket connections
+const clients = new Map();
 
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    const data = JSON.parse(message);
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        switch (data.type) {
+            case 'login':
+                clients.set(data.username, ws);
+                break;
+            case 'message':
+                const receiverWs = clients.get(data.receiver);
+                if (receiverWs) {
+                    receiverWs.send(JSON.stringify({
+                        type: 'message',
+                        sender: data.sender,
+                        message: data.message
+                    }));
+                }
+                break;
+            case 'offer':
+            case 'answer':
+            case 'candidate':
+                const peerWs = clients.get(data.peer);
+                if (peerWs) {
+                    peerWs.send(JSON.stringify(data));
+                }
+                break;
+            default:
+                break;
+        }
+    });
 
-    if (data.type === "login") {
-      users[data.username] = ws;
-      ws.username = data.username;
-      ws.send(JSON.stringify({ type: "login", success: true }));
-    } else if (data.type === "message") {
-      const recipient = users[data.to];
-      if (recipient) {
-        recipient.send(
-          JSON.stringify({
-            type: "message",
-            from: ws.username,
-            message: data.message,
-          })
-        );
-      } else {
-        ws.send(JSON.stringify({ type: "error", message: "User not found" }));
-      }
-    }
-  });
-
-  ws.on("close", () => {
-    if (ws.username) {
-      delete users[ws.username];
-    }
-  });
+    ws.on('close', () => {
+        for (let [username, clientWs] of clients.entries()) {
+            if (clientWs === ws) {
+                clients.delete(username);
+                break;
+            }
+        }
+    });
 });
 
-console.log("WebSocket server is running on ws://localhost:8080");
+console.log('WebSocket server is running on ws://localhost:8080');
